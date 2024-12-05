95c4258669504b28ee565212c03bf658741cb743
Ranjitha737 <vvce22cse0138@vvce.ac.in>
2024-12-05 16:01:15 +0530

    Update java.java

--- java.java
+++ java.java
@@ -1,5 +1,8 @@
 public class Java{
   public static void main(String[]args){
     System.out.println("hello");
+     System.out.println("mysore");
+     System.out.println("bangalore");
+     System.out.println("vvce");
   }
 }
